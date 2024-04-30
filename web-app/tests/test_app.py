7caeabb030214f14c4a48a7cfdd3e28f7a7d8dbd
fctico11 <fac322@nyu.edu>
2024-04-30 16:47:26 -0400

    test fixes for web-app

--- web-app/tests/test_app.py
+++ web-app/tests/test_app.py
@@ -1,9 +1,9 @@
+from unittest.mock import patch
 import pytest
 import sys
 sys.path.append('web-app')
 from app import app
 import base64
-import json
 from io import BytesIO
 
 @pytest.fixture
@@ -18,9 +18,14 @@ def test_home_route(client):
     response = client.get('/')
     assert response.status_code == 200
 
-def test_save_picture(client):
-    """Ensure the /save_picture route works"""
-    #base64 string for a 1x1 pixel black png image
+@patch('requests.post')  
+def test_save_picture(mock_post, client):
+    """ensure the save picture route works"""
+    # Mock the POST request to mlclient
+    mock_post.return_value.status_code = 202  # Assume mlclient returns 202 when processing is started
+    mock_post.return_value.json.return_value = {'status': 'Processing started'}
+
+    # Base64 string for a 1x1 pixel black png image
     fake_image_base64 = "iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAB3RJTUUH5QgFCSwYFiGXGgAAAApJREFUCNdjYGBgAAAABAABJzQnCgAAAABJRU5ErkJggg=="
     fake_image = BytesIO()
     fake_image.write(base64.b64decode(fake_image_base64))
@@ -30,10 +35,10 @@ def test_save_picture(client):
         'image': 'data:image/png;base64,' + base64.b64encode(fake_image.getvalue()).decode('utf-8')
     }
 
-    #send POST request to the server
+    # Send POST request to the server
     response = client.post('/save_picture', data=data)
 
-    #check the response
+    # Check the response
     assert response.status_code == 200, "Response status should be 200"
     assert 'Unknown Plant' in response.data.decode('utf-8'), "Check the HTML content for plant name"
 
